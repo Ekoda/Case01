1ae12a621778c8699f7bab6f19a3ffd569e55f95
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2022-01-25 23:43:48 +0100

    Update case.py

--- case.py
+++ case.py
@@ -1,8 +1,10 @@
 
-# Users have access to three functionalities in console
-# f("S" + n) = Sell n of inventory
-# f("I" + n) = Add n to inventory
-# f("L") = Show current inventory
+# Uppgiften består av att skriva en konsolapplikation för att hantera lagersaldon.
+# Användargränssnittet ska innehålla tre funktioner:
+#   1) Att sälja x antal. Görs genom att man skriver “S” och så antalet + enter, t.ex. ”S5”.
+#   2) Att inleverera x antal. Görs genom att man skriver “I” (stort i) och så antalet + enter, t.ex. ”I5”.
+#   3) Visa aktuellt lager. Görs genom att man skriver ”L” + enter.
+# När en försäljning eller inleverans görs ska lagersaldot räknas ner eller upp med angivet antal. När programmet startar är saldot alltid 0 och data behöver inte lagras på något annat sätt än i primärminnet.
 
 
 # Logic ---
@@ -47,32 +49,32 @@ def manage(user_input, data):
 
 
 # Run the program / Console presentation ---
+if __name__ == '__main__':
+    # Introduces the functionality of the program in console
+    print('\nValid commands:')
+    print("* Show inventory amount: 'L'")
+    print("* Add amount to inventory: I followed by amount e.g 'I20'")
+    print("* Remove amount from inventory: S followed by amount e.g 'S7'")
+    print("* 'Q' to exit program\n")
 
-# Introduces the functionality of the program in console
-print('\nValid commands:')
-print("* Show inventory amount: 'L'")
-print("* Add amount to inventory: I followed by amount e.g 'I20'")
-print("* Remove amount from inventory: S followed by amount e.g 'S7'")
-print("* 'Q' to exit program\n")
+    active = True
+    inventory = 0 # The example inventory we want to change
 
-active = True
-inventory = 0 # The example inventory we want to change
+    while active:
+        user_input = input("Command: ")
+        if user_input == 'Q': 
+            print('Program exited\n')
+            break
 
-while active:
-    user_input = input("Command: ")
-    if user_input == 'Q': 
-        print('Program exited\n')
-        break
+        processed_input = manage(user_input, inventory)
 
-    processed_input = manage(user_input, inventory)
-
-    if processed_input == 'Invalid':
-        print("\n\nInvalid command!\n\nValid commands:\n*Show inventory amount: L\n*Add amount to inventory: I followed by amount, e.g 'I20'\n*Remove amount from inventory: S followed by amount, e.g 'S5'\n")
-    elif user_input[0] == 'L':
-        print('There are ' + str(processed_input) + ' item(s) in inventory.\n')
-    elif user_input[0] == 'S':
-        inventory = processed_input # Updates inventory in accordance with principles of functional programming as each data entity is immutable.
-        print('Removed ' + str(user_input[1:]) + ' item(s) from inventory.\n')
-    elif user_input[0] == 'I':
-        inventory = processed_input # Updates inventory in accordance with principles of functional programming as each data entity is immutable.
-        print('Added ' + str(user_input[1:]) + ' item(s) to inventory.\n')
+        if processed_input == 'Invalid':
+            print("\n\nInvalid command!\n\nValid commands:\n*Show inventory amount: L\n*Add amount to inventory: I followed by amount, e.g 'I20'\n*Remove amount from inventory: S followed by amount, e.g 'S5'\n")
+        elif user_input[0] == 'L':
+            print('There are ' + str(processed_input) + ' item(s) in inventory.\n')
+        elif user_input[0] == 'S':
+            inventory = processed_input # Updates inventory in accordance with principles of functional programming as each data entity is immutable.
+            print('Removed ' + str(user_input[1:]) + ' item(s) from inventory.\n')
+        elif user_input[0] == 'I':
+            inventory = processed_input # Updates inventory in accordance with principles of functional programming as each data entity is immutable.
+            print('Added ' + str(user_input[1:]) + ' item(s) to inventory.\n')
